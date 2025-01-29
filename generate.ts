103da4d715c2af0e82f830b12ee7b54cb0ea1cb3
Nick K <nick.kruchinin@gmail.com>
2025-01-29 22:47:19 +0300

    fix(plugin): Update output path resolution tu support Node.js v18

--- generate.ts
+++ generate.ts
@@ -2,7 +2,8 @@
 // so it can be consumed by Tailwindcss 4 via the `@import` directive.
 
 import {writeFile} from "node:fs/promises"
-import {join} from "node:path"
+import {dirname, join} from "node:path"
+import {fileURLToPath} from "node:url"
 
 import colors from "picocolors"
 
@@ -11,7 +12,7 @@ import {plugin} from "./src/css/plugin.ts"
 // Write plugin styles
 await Promise.all(
   ["lib/device.css", "index.css"].map(async filename => {
-    const path = join(import.meta.dirname, filename)
+    const path = join(dirname(fileURLToPath(import.meta.url)), filename)
 
     console.log(`${colors.blue("PLUGIN")} Writing CSS output to ${path}`)
 
